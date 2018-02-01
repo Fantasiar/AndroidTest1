fbbec6315472f2d8cf62f6eda5c89a7a05432047
Fouter <1420126933@qq.com>
2018-02-01 17:32:23 +0800

    the second push

--- app/src/main/java/com/fouter/thesecondproject/MainActivity.kt
+++ app/src/main/java/com/fouter/thesecondproject/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.content.Intent
 import android.net.Uri
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
