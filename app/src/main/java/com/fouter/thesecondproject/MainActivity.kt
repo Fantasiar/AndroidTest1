e75254db6eaec7ca16ad011e38137576823fde33
Fouter <1420126933@qq.com>
2018-02-01 17:06:48 +0800

    the first push

---
+++ app/src/main/java/com/fouter/thesecondproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.fouter.thesecondproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
